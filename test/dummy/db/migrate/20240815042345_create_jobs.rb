6371e9aaf0c728177c7f8e431d448947db305f7c
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-08-15 01:15:15 -0500

    Fix test/dummy migration options
    Status columns should generally not be null, and should be indexed. A
    default value of e.g. "Initializing" makes sense from the database.

--- test/dummy/db/migrate/20240815042345_create_jobs.rb
+++ test/dummy/db/migrate/20240815042345_create_jobs.rb
@@ -1,7 +1,7 @@
 class CreateJobs < ActiveRecord::Migration[7.2]
   def change
     create_table :jobs do |t|
-      t.string :status
+      t.string :status, null: false, default: "Initializing", index: true
 
       t.timestamps
     end
